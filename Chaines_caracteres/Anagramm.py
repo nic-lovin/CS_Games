4d499fdc7d65cb3c3e0f3f638f58da9b5742e623
nic-lovin <nic-lovin@users.noreply.github.com>
2016-01-17 18:39:50 -0500

    Delete Anagramm.py

--- Chaines_caracteres/Anagramm.py
+++
@@ -1,7 +0,0 @@
-if __name__ == '__main__':
-    firstWord = input()
-    secondWord = input()
-    answer = sorted(firstWord.intersection(secondWord))
-    firstWord = len(firstWord) - len(sorted(firstWord.intersection(answer)))
-    secondWord = len(secondWord) - len(sorted(secondWord.intersection(answer)))
-    print(firstWord + secondWord)
