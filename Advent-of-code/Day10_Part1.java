eeaa89e213bb9bbac7462bafdc4078d7dcef8077
nic-lovin <nic-lovin@users.noreply.github.com>
2016-01-18 15:49:30 -0500

    Create Day10_Part1.java

---
+++ Advent-of-code/Day10_Part1.java
@@ -0,0 +1,31 @@
+public class Solution {
+    private static String generateLine(String s1) {
+        int size = s1.length();
+        String newString = "";
+        int i = 0;
+        while (i < size) {
+            int j = i;
+            int offset = 1;
+            while ((i + offset < size) && (s1.charAt(i) == s1.charAt(i + offset))) {
+                offset++;
+            }
+            if (offset > 1)  {
+                i += offset;
+            } else {
+                i++;
+            }
+            newString = newString + offset + s1.charAt(j);
+        }
+
+        return newString;
+    }
+
+    public static void main(String[] args) {
+        String s1 = generateLine("1321131112");
+        for (int i = 0; i < 39; i++) {
+            s1 = generateLine(s1);
+            System.out.println(i);
+        }
+        System.out.println(s1.length());
+    }
+}
