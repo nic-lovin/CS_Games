9880c7eee7e4c3c3bb7b86c231b5d34f4a489912
nic-lovin <nic-lovin@users.noreply.github.com>
2016-01-17 22:41:06 -0500

    Create Day10_Part1.py

---
+++ Advent-of-code/Day10_Part1.py
@@ -0,0 +1,10 @@
+import itertools
+
+table = ...
+print(table)
+count = 0
+for i in range(0, len(table)+1):
+  for subset in itertools.combinations(table, i):
+    if sum(subset) == 150:
+        count += 1
+print(count)
